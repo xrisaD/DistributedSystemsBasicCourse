e496b6367916e970ab9e9db02012fc923a0150aa
Chrysa <xrdikonimaki@hotmail.com>
2021-09-03 08:41:26 +0200

    bank example

---
+++ ErlangPractice/bank.erl
@@ -0,0 +1,10 @@
+-module(bank).
+-export([start/1]).
+
+start(Balance) -> 
+    spawn(fun() -> server(Balance) end).
+server(Balance) -> receive
+                    {deposit, X} -> server(Balance+X);
+                    {withdraw, X} -> server(Balance-X);
+                    quit -> ok
+                   end.
