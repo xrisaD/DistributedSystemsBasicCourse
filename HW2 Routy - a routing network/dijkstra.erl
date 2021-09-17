32a4fa3c10d9596a02b6abf642f5de9eacfe852b
Chrysa <xrdikonimaki@hotmail.com>
2021-09-17 18:57:59 +0200

    debugging dijksra functions, added update function

--- HW2 Routy - a routing network/dijkstra.erl
+++ HW2 Routy - a routing network/dijkstra.erl
@@ -1,8 +1,12 @@
 -module(dijkstra).
--export([entry/2, replace/4]).
+-export([entry/2, replace/4, update/4]).
 
 % returns the length of the shortest path to the node or 0 if the node is not found.
-entry(Node, Sorted) -> {NodeName, Length, Gateway} = lists:keyfind(Node, 1, Sorted), Length.
+entry(Node, Sorted) -> 
+    case lists:keyfind(Node, 1, Sorted) of
+        false -> 0;
+        {_, Length, _} -> Length
+    end.
 
 % replaces the entry for Node in Sorted with a new entry having a new length N and Gateway. The resulting list should of course be sorted.
 replace(Node, N, Gateway, Sorted) ->
@@ -14,6 +18,19 @@ replace(Node, N, Gateway, Sorted) ->
         true ->
             % delete the old element
             NewSorted = lists:delete(Found, Sorted),
-            % add the new element and sort the new list
-            lists:keysort(1, NewSorted ++ [{Node, N, Gateway}])
+            % add the new element and 
+            % sort the new list based on the second element (the distance)
+            lists:keysort(2, NewSorted ++ [{Node, N, Gateway}])
+    end.
+
+% update the list Sorted given
+% the information that Node can be reached in N hops using Gateway.
+% If no entry is found then no new entry is added. Only if we have a
+% better (shorter) path should we replace the existing entry
+update(Node, N, Gateway, Sorted) -> 
+    % get the length of the existing path
+    ExistingN = entry(Node, Sorted),
+    % if the entry exists and the new path is shorter than the old path, replace the old one with the new one
+    if 
+        (ExistingN /= 0) and (N < ExistingN) -> replace(Node, N, Gateway, Sorted)
     end.
