02b3b2ec58ef36f317d3e8ddcd6ca396451760d9
Chrysa <xrdikonimaki@hotmail.com>
2021-09-17 18:32:05 +0200

    routy: dijkstra, first 2 functions uppdated

--- HW2 Routy - a routing network/dijkstra.erl
+++ HW2 Routy - a routing network/dijkstra.erl
@@ -5,8 +5,15 @@
 entry(Node, Sorted) -> {NodeName, Length, Gateway} = lists:keyfind(Node, 1, Sorted), Length.
 
 % replaces the entry for Node in Sorted with a new entry having a new length N and Gateway. The resulting list should of course be sorted.
-replace(Node, N, Gateway, Sorted) -> 
-    % delete the old element
-    NewSorted = lists:keydelete(Node, 1, Sorted),
-    % add the new element and sort the new list
-    lists:keysort(1, NewSorted ++ [{Node, N, Gateway}]).
+replace(Node, N, Gateway, Sorted) ->
+    % check that Node exists in list
+    Found = lists:keyfind(Node, 1, Sorted),
+    if 
+         Found == false -> 
+            Sorted; % return the same list
+        true ->
+            % delete the old element
+            NewSorted = lists:delete(Found, Sorted),
+            % add the new element and sort the new list
+            lists:keysort(1, NewSorted ++ [{Node, N, Gateway}])
+    end.
